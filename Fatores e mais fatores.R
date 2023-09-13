d65e099aa92bce382ebe7cc330977f3c82e9f4a9
Alessandra Guimaraes <alessandracsg@gmail.com>
2023-09-13 18:41:42 -0300

    Atividade 01 Fatores e mais fatores

---
+++ Fatores e mais fatores.R
@@ -0,0 +1,29 @@
+pacman::p_load(janitor,ade4,arules,forcats)
+
+facebook <- read.table(
+  "https://raw.githubusercontent.com/hugoavmedeiros/ciencia_politica_com_r/master/bases_originais/dataset_Facebook.csv", 
+  sep=";", 
+  header = T)
+
+str(facebook)
+
+for(i in 2:7) { facebook[,i] <- as.factor(facebook[,i]) }
+
+str(facebook)
+
+factorsFacebook <- unlist(lapply(facebook, is.factor))  
+facebookFactor <- facebook[ , factorsFacebook]
+
+str(facebookFactor)
+
+inteirosFacebook <- unlist(lapply(facebook, is.integer))  
+facebookInteiros <- facebook[ , inteirosFacebook]
+
+str(facebookInteiros)
+
+
+facebookInteiros$Page.total.likes.Disc <- discretize(facebookInteiros$Page.total.likes, method = "interval", breaks = 3, labels = c('mais frequente', 'segundo mais frequente', 'outros'))
+
+facebookInteiros <- facebookInteiros %>% clean_names() 
+
+facebookInteiros %>% names()
